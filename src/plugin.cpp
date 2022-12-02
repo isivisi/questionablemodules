0954938c228bc44bda2b2c228af01a310b74befc
isivisi <johniannandrea@gmail.com>
2022-12-01 20:29:06 -0500

    working visuals

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,19 +3,11 @@
 
 Plugin* pluginInstance;
 
-/*
-    Param: Read with params[...].getValue()
-    Input: Read with inputs[...].getVoltage()
-    Output: Write with outputs[...].setVoltage(voltage)
-    Light: Write with lights[...].setBrightness(brightness)
-*/
-
-
 void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	p->addModel(modelModule);
+	p->addModel(modelNrandomizer);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
