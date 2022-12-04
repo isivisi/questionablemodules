c9434b2591d442965f4b7c199abed31c73088d8b
isivisi <johniannandrea@gmail.com>
2022-12-03 19:17:38 -0500

    moved imagepanel to its own file

--- src/discombobulator.cpp
+++ src/discombobulator.cpp
@@ -1,4 +1,5 @@
 #include "plugin.hpp"
+#include "imagepanel.cpp"
 #include <vector>
 #include <list>
 #include <random>
@@ -145,51 +146,14 @@ struct Discombobulator : Module {
 	}
 };
 
-struct MSMPanel : TransparentWidget {
-  NVGcolor backgroundColor = componentlibrary::SCHEME_LIGHT_GRAY;
-  float scalar = 1.0;
-  std::string imagePath;
-	void draw(const DrawArgs &args) override {
-      std::shared_ptr<Image> backgroundImage = APP->window->loadImage(imagePath);
-	  nvgBeginPath(args.vg);
-	  nvgRect(args.vg, 0.0, 0.0, box.size.x, box.size.y);
-
-	  // Background color
-	  if (backgroundColor.a > 0) {
-	    nvgFillColor(args.vg, backgroundColor);
-	    nvgFill(args.vg);
-	  }
-
-	  // Background image
-	  if (backgroundImage) {
-	    int width, height;
-	    nvgImageSize(args.vg, backgroundImage->handle, &width, &height);
-	    NVGpaint paint = nvgImagePattern(args.vg, 0.0, 0.0, width/scalar, height/scalar, 0.0, backgroundImage->handle, 1.0);
-	    nvgFillPaint(args.vg, paint);
-	    nvgFill(args.vg);
-	  }
-
-	  // Border
-	  NVGcolor borderColor = componentlibrary::SCHEME_LIGHT_GRAY; //nvgRGBAf(0.5, 0.5, 0.5, 0.5);
-	  nvgBeginPath(args.vg);
-	  nvgRect(args.vg, 0.5, 0.5, box.size.x - 1.0, box.size.y - 1.0);
-	  nvgStrokeColor(args.vg, borderColor);
-	  nvgStrokeWidth(args.vg, 1.0);
-	  nvgStroke(args.vg);
-
-	  Widget::draw(args);
-	}
-};
-
-
 struct DiscombobulatorWidget : ModuleWidget {
-	MSMPanel *backdrop;
+	ImagePanel *backdrop;
 
 	DiscombobulatorWidget(Discombobulator* module) {
 		setModule(module);
 		//setPanel(createPanel(asset::plugin(pluginInstance, "res/nrandomizer.svg")));
 
-		backdrop = new MSMPanel();
+		backdrop = new ImagePanel();
 		backdrop->box.size = Vec(9 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
 		backdrop->imagePath = asset::plugin(pluginInstance, "res/backdrop-dis.png");
 		backdrop->scalar = 3.5;
