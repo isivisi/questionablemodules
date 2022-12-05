019be384c8d851b55ea45c69ecaa21d06167f44d
isivisi <johniannandrea@gmail.com>
2022-12-04 23:47:35 -0500

    fixed plugin not found

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -9,7 +9,7 @@ void init(Plugin* p) {
 	// Add modules here
 	p->addModel(modelNandomizer);
 	p->addModel(modelDiscombobulator);
-	p->addModel(modelTreequencer);
+	//p->addModel(modelTreequencer);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
