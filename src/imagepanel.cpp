15bf3e08a80412c13ebd8ac28f2f8c4832b34a29
isivisi <johniannandrea@gmail.com>
2022-12-06 14:14:12 -0500

    removed glow from imagepanel

--- src/imagepanel.cpp
+++ src/imagepanel.cpp
@@ -5,13 +5,18 @@ struct ImagePanel : TransparentWidget {
   bool glows = false;
   std::string imagePath;
 	void draw(const DrawArgs &args) override {
+	  std::shared_ptr<Image> backgroundImage = APP->window->loadImage(imagePath);
 	  nvgBeginPath(args.vg);
 	  nvgRect(args.vg, 0.0, 0.0, box.size.x, box.size.y);
 
 	  // Background image
-	  if (!glows) {
-	    drawBackground(args.vg);
-	  }
+	  	if (backgroundImage) {
+			int width, height;
+			nvgImageSize(args.vg, backgroundImage->handle, &width, &height);
+			NVGpaint paint = nvgImagePattern(args.vg, 0.0, 0.0, width/scalar, height/scalar, 0.0, backgroundImage->handle, 1.0);
+			nvgFillPaint(args.vg, paint);
+			nvgFill(args.vg);
+		}
 
 	  // Border
 	  NVGcolor borderColor = componentlibrary::SCHEME_LIGHT_GRAY; //nvgRGBAf(0.5, 0.5, 0.5, 0.5);
@@ -22,25 +27,5 @@ struct ImagePanel : TransparentWidget {
 	  nvgStroke(args.vg);
 
 	  Widget::draw(args);
-	}
-
-	void drawLayer(const DrawArgs &args, int layer) override {
-		if (layer == 1) {
-			// Background image
-			if (glows) {
-				drawBackground(args.vg);
-			}
-		}
-	}
-
-	void drawBackground(NVGcontext* vg) {
-		int width, height;
-		std::shared_ptr<Image> backgroundImage = APP->window->loadImage(imagePath);
-		if (backgroundImage) {
-			nvgImageSize(vg, backgroundImage->handle, &width, &height);
-			NVGpaint paint = nvgImagePattern(vg, 0.0, 0.0, width/scalar, height/scalar, 0.0, backgroundImage->handle, 1.0);
-			nvgFillPaint(vg, paint);
-			nvgFill(vg);
-		}
 	}
 };
