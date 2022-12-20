9f3cf9677728afbacbf050f148d414d57de7bfe6
isivisi <johniannandrea@gmail.com>
2022-12-20 12:10:04 -0500

    fixed crash

--- src/treequencer.cpp
+++ src/treequencer.cpp
@@ -556,7 +556,7 @@ struct NodeDisplay : Widget {
 		menu->addChild(rack::createMenuLabel("Node Output:"));
 
 		ui::TextField* outparam = new QTextField([=](std::string text) {
-			if (isInteger(text)) mod->onAudioThread([=](){ node->setOutput(std::stoi(text)-1); });
+			if (text.length() < 4 && isInteger(text)) mod->onAudioThread([=](){ node->setOutput(std::stoi(text)-1); });
 		});
 		outparam->box.size.x = 100;
 		outparam->text = std::to_string(node->output + 1);
@@ -565,7 +565,7 @@ struct NodeDisplay : Widget {
 		menu->addChild(rack::createMenuLabel("Node Chance:"));
 
 		ui::TextField* param = new QTextField([=](std::string text) { 
-			if (isNumber(text)) mod->onAudioThread([=](){ node->setChance((float)::atof(text.c_str())); });
+			if (text.length() < 4 && isNumber(text)) mod->onAudioThread([=](){ node->setChance((float)::atof(text.c_str())); });
 		});
 		param->box.size.x = 100;
 		param->text = std::to_string(node->chance);
