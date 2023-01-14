4420080d083b1f2a9cd80831c2f482b2648886d7
isivisi <johniannandrea@gmail.com>
2023-01-14 14:15:56 -0500

    fixed script issue

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,16 @@ async function importHTML(file) {
     const resp = await fetch(file);
     const html = await resp.text();
     document.body.insertAdjacentHTML("beforeend", html);
+    runEmbeddedScripts(html);
+}
+
+function runEmbeddedScripts(text) {
+    var root = document.createElement('root');
+    root.innerHTML = text;
+
+    var scripts = root.getElementsByTagName('script');
+    
+    for (var i = 0; i < scripts.length; i++) {
+        eval(scripts[i].innerHTML);
+    }
 }
