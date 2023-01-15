d76ea7da105fa9106e7d431e0f32c2bf64024bf4
isivisi <johniannandrea@gmail.com>
2023-01-14 23:05:51 -0500

    Previous verison doc links, thanks to githack

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,17 @@
 // Settings
 var DocVersion = "v2.1.5";
+var previousVersions = [
+    {
+        verison: "v2.1.4",
+        commit: "ae440e0e43fe738f03537fc4261bc3fdc9fc9ad1"
+    }
+]
 
-async function importHTML(file) {
+async function importHTML(file, parent=undefined) {
     const resp = await fetch(file);
     const html = await resp.text();
-    document.body.insertAdjacentHTML("beforeend", html);
+    if (parent == undefined) document.body.insertAdjacentHTML("beforeEnd", html);
+    else parent.insertAdjacentHTML("beforeEnd", html)
     runEmbeddedScripts(html);
 }
 
