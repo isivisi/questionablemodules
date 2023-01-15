947c7f05795170139834000ee44fac248df6a336
isivisi <johniannandrea@gmail.com>
2023-01-15 02:13:39 -0500

    wip auto sequence generation

--- src/treequencer.cpp
+++ src/treequencer.cpp
@@ -72,6 +72,35 @@ bool isNumber(std::string s)
 	return char_pos == s.size(); // must reach the ending 0 of the string 
 }
 
+struct ScaleDef {
+	std::string name;
+	std::vector<int> notes;
+
+	// probabilities[note] = {anotherNote: probability, ...}
+	//std::map<int, std::map<int, float>> probabilities;
+
+	std::vector<int> generateRandom(int size) {
+		std::vector<int> sequence;
+
+		for (int i = 0; i < size; i++) {
+			sequence.push_back(notes[randomInteger(0, notes.size()-1)]);
+		}
+
+		return sequence;
+	}
+};
+
+// All starting at C
+std::vector<ScaleDef> scales = {
+	ScaleDef{"Chromatic", {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}}, // C(0), C#(1), D(2), D#(3), E(4), F(5), F#(6), G(7), G#(8), A(9), A#(10), B(11)
+	ScaleDef{"Major", {0, 2, 4, 5, 7, 9, 11}}, // C, D, E, F, G, A, B
+	ScaleDef{"Minor", {0, 2, 3, 5, 7, 8, 10}} // C, D, D#, F, G, G#, A#
+	ScaleDef{"Pentatonic", {0, 2, 5, 7, 10}}, // C, D, F, G, A#
+	ScaleDef{"Blues", {0, 3, 5, 6, 7, 10}}, // C, D#, F, F#, G, A#
+	ScaleDef{"Dorian", {0, 2, 3, 5, 7, 9, 10}}, // C, D, D#, F, G, A, A#
+	ScaleDef{"Mixolydian", {0, 2, 4, 5, 7, 9, 10}} // C, D, E, F, G, A, A#
+}
+
 // A node in the tree
 struct Node {
   	int output = 0;
