9eb6b10e04fcc0f1768598c8d99b5341c580b72f
isivisi <johniannandrea@gmail.com>
2023-01-17 20:04:25 -0500

    cleaned up settings logic

--- src/settings.hpp
+++ src/settings.hpp
@@ -27,11 +27,30 @@ struct UserSettings {
 
     template <typename T>
     T getSetting(std::string setting) {
+
+        if constexpr (std::is_same<T, int>::value) return json_integer_value(json_object_get(readSettings(), setting.c_str()));
+        if constexpr (std::is_same<T, float>::value) return json_real_value(json_object_get(readSettings(), setting.c_str()));
+        if constexpr (std::is_same<T, std::string>::value) return json_string_value(json_object_get(readSettings(), setting.c_str()));
+
         throw std::runtime_error("QuestionableModules::UserSettings::getJsonSetting function for type not defined. :(");
     }
 
     template <typename T>
     void setSetting(std::string setting, T value) {
+        json_t* v = nullptr;
+
+        if constexpr (std::is_same<T, int>::value) v = json_integer(value);
+        if constexpr (std::is_same<T, float>::value) v = json_real(value);
+        if constexpr (std::is_same<T, std::string>::value) v = json_string(value.c_str());
+
+        if (v) {
+            json_t* settings = readSettings();
+            json_object_set(settings, setting.c_str(), v);
+            saveSettings(settings);
+
+            return;
+        }
+        
         throw std::runtime_error("QuestionableModules::UserSettings::setJsonSetting function for type not defined. :(");
     }
 
@@ -66,10 +85,3 @@ struct UserSettings {
     }
     
 };
-
-template<> int UserSettings::getSetting<int>(std::string);
-template<> float UserSettings::getSetting<float>(std::string);
-template<> std::string UserSettings::getSetting<std::string>(std::string);
-template<> void UserSettings::setSetting<int>(std::string, int);
-template<> void UserSettings::setSetting<float>(std::string, float);
-template<> void UserSettings::setSetting<std::string>(std::string, std::string);
