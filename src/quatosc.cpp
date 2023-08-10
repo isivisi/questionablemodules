6643154bedf53a5a2f600a2a82fadc09e5cf2df4
isivisi <jiannandrea@gmail.com>
2023-08-09 20:32:27 -0400

    fixed lighting stuff

--- src/quatosc.cpp
+++ src/quatosc.cpp
@@ -433,13 +433,30 @@ struct QuatDisplay : Widget {
 
 // our three way switch for mono, stereo, and stereo - middle
 struct SLURPStereoSwitch : QuestionableParam<SvgSwitch> {
+	bool allowDraw = false;
+
 	SLURPStereoSwitch() {
 		QuestionableParam();
+
+		fb->removeChild(shadow); // we don't need this
 
 		momentary = false;
 		addFrame(Svg::load(asset::plugin(pluginInstance, "res/slurpMono.svg")));
 		addFrame(Svg::load(asset::plugin(pluginInstance, "res/slurpFullStereo.svg")));
 		addFrame(Svg::load(asset::plugin(pluginInstance, "res/slurpSides.svg")));
+	}
+
+	// force draw on light layer
+	void drawLayer(const DrawArgs &args, int layer) override {
+		if (layer == 1) {
+			allowDraw = true;
+			draw(args);
+			allowDraw = false;
+		}
+	}
+
+	void draw(const DrawArgs &args) override {
+		if (allowDraw) SvgSwitch::draw(args);
 	}
 
 };
