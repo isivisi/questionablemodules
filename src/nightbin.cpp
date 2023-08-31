e363ec177bdd9dfc8ea88cfd93732d0ea7b31923
John <jiannandrea@gmail.com>
2023-08-31 01:49:17 -0400

    wip

--- src/nightbin.cpp
+++ src/nightbin.cpp
@@ -11,7 +11,7 @@
 #include <mutex>
 #include <plugin.hpp>
 
-const int MODULE_SIZE = 6;
+const int MODULE_SIZE = 8;
 
 
 
@@ -71,7 +71,7 @@ struct NightBinWidget : QuestionableWidget {
 	void setText() {
 		NVGcolor c = nvgRGB(255,255,255);
 		color->textList.clear();
-		color->addText("NIGHTBIN", "OpenSans-ExtraBold.ttf", c, 24, Vec((MODULE_SIZE * RACK_GRID_WIDTH) / 2, 25));
+		color->addText("NIGHT-BIN", "OpenSans-ExtraBold.ttf", c, 24, Vec((MODULE_SIZE * RACK_GRID_WIDTH) / 2, 25));
 		color->addText("·ISI·", "OpenSans-ExtraBold.ttf", c, 28, Vec((MODULE_SIZE * RACK_GRID_WIDTH) / 2, RACK_GRID_HEIGHT-13));
 	}
 
@@ -80,7 +80,7 @@ struct NightBinWidget : QuestionableWidget {
 
 		backdrop = new ImagePanel();
 		backdrop->box.size = Vec(MODULE_SIZE * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
-		//backdrop->imagePath = asset::plugin(pluginInstance, "res/backdrop.jpg");
+		backdrop->imagePath = asset::plugin(pluginInstance, "res/workbench.jpg");
 		backdrop->scalar = 3.5;
 		backdrop->visible = true;
 
@@ -101,7 +101,7 @@ struct NightBinWidget : QuestionableWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		startQueryThread();
+		//startQueryThread();
 	}
 
     struct QRemotePluginInfo {
@@ -251,7 +251,7 @@ struct NightBinWidget : QuestionableWidget {
 
 		menu->addChild(new MenuSeparator);
 		if (gathering.try_lock()) {
-			menu->addChild(createMenuItem("Query for Updates", "⭯",[=]() { startQueryThread(); }));
+			menu->addChild(createMenuItem("Query for Updates", "",[=]() { startQueryThread(); }));
 			for (plugin::Plugin* plugin : plugins) {
 				menu->addChild(createMenuItem(plugin->name, plugin->version, [=]() {
 					
