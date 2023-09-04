de29d0c653d8d4eaffdb1024c5f391f0d852062b
isivisi <jiannandrea@gmail.com>
2023-09-04 16:50:53 -0400

    wip

---
+++ src/greenscreen.cpp
@@ -0,0 +1,75 @@
+#include "plugin.hpp"
+#include "imagepanel.cpp"
+#include "colorBG.hpp"
+#include "questionableModule.hpp"
+#include <vector>
+#include <algorithm>
+
+const int MODULE_SIZE = 3;
+
+struct Greenscreen : QuestionableModule {
+	enum ParamId {
+		PARAMS_LEN
+	};
+	enum InputId {
+		INPUTS_LEN
+	};
+	enum OutputId {
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHTS_LEN
+	};
+
+	Greenscreen() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		
+	}
+
+	void process(const ProcessArgs& args) override {
+
+		
+
+	}
+
+};
+
+struct GreenscreenWidget : QuestionableWidget {
+
+	void setText() {
+		NVGcolor c = nvgRGB(255,255,255);
+		color->textList.clear();
+		color->addText("GREENSCREEN", "OpenSans-ExtraBold.ttf", c, 24, Vec((MODULE_SIZE * RACK_GRID_WIDTH) / 2, 25));
+		color->addText("·ISI·", "OpenSans-ExtraBold.ttf", c, 28, Vec((MODULE_SIZE * RACK_GRID_WIDTH) / 2, RACK_GRID_HEIGHT-13));
+	}
+
+	GreenscreenWidget(Greenscreen* module) {
+		setModule(module);
+
+		backdrop = new ImagePanel();
+		backdrop->box.size = Vec(MODULE_SIZE * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+		//backdrop->imagePath = asset::plugin(pluginInstance, "res/backdrop.jpg");
+		backdrop->scalar = 3.5;
+		backdrop->visible = true;
+
+		color = new ColorBG(Vec(MODULE_SIZE * RACK_GRID_WIDTH, RACK_GRID_HEIGHT));
+		color->drawBackground = false;
+		setText();
+
+		backgroundColorLogic(module);
+		
+		setPanel(backdrop);
+		addChild(color);
+
+		//addChild(new QuestionableDrawWidget(Vec(18, 100), [module](const DrawArgs &args) {
+		//}));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+	}
+
+};
+
+Model* modelGreenscreen = createModel<Greenscreen, GreenscreenWidget>("greenscreen");
