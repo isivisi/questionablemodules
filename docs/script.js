b46b527abffaaa669fe39e56f4b7fbcda0d7a3ba
isivisi <jiannandrea@gmail.com>
2023-09-05 22:26:48 -0400

    proper commit

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ var DocVersion = "v2.1.10";
 var previousVersions = [
     {
         version: "v2.1.8 - v2.1.9",
-        commit: "doc2.1.8"
+        commit: "doc2.1.9"
     },
     {
         verison: "v2.1.7",
