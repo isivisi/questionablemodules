3ee40e2a56db5236846352788ede011b8047d6c9
John Iannandrea <jiannandrea@gmail.com>
2023-10-02 14:16:46 -0400

    wip

--- src/treequencer.cpp
+++ src/treequencer.cpp
@@ -432,6 +432,12 @@ struct Treequencer : QuestionableModule {
 		}
 	}
 
+	void clearHistory() {
+		history.clear();
+		historyPos = 0;
+		historyDirty = false;
+	}
+
 	void pushHistory(json_t* state = nullptr) {
 		if (historyPos != history.size()) history.erase(history.begin() + historyPos-1, history.end());
 		history.push_back(state ? state : rootNode.toJson());
@@ -441,6 +447,7 @@ struct Treequencer : QuestionableModule {
 
 	void historyGoBack() {
 		if (history.empty()) return;
+		if (historyPos == 1) return;
 		if (historyPos == history.size() && historyDirty) { // save latest changes before going back
 			pushHistory();
 			historyDirty = false;
@@ -492,6 +499,8 @@ struct Treequencer : QuestionableModule {
 		
 		rootNode.enabled = true;
 		activeNode = &rootNode;
+
+		pushHistory();
 		
 	}
 
@@ -672,6 +681,10 @@ struct Treequencer : QuestionableModule {
 
 		if (json_t* rn = json_object_get(rootJ, "rootNode")) setRootNodeFromJson(rn);
 
+		clearHistory();
+		pushHistory();
+		historyDirty = false;
+
 	}
 
 
@@ -779,7 +792,7 @@ struct TreequencerHistoryButton : TreequencerButton {
 
 	void step() override {
 		if (!module) return;
-		disabled = isBack ? module->history.size() == 0 || module->historyPos < 1 : module->historyPos >= module->history.size();
+		disabled = isBack ? module->history.size() == 0 || module->historyPos <= 1 : module->historyPos >= module->history.size();
 	}
 	
 	void onButton(const ButtonEvent& e) override {
