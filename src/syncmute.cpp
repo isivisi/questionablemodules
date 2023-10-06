bfb5ac6f99d55c26483231fb3abdf2c00e37341a
John <jiannandrea@gmail.com>
2023-10-06 11:28:13 -0400

    wip

--- src/syncmute.cpp
+++ src/syncmute.cpp
@@ -9,14 +9,46 @@ const int MODULE_SIZE = 8;
 
 struct SyncMute : QuestionableModule {
 	enum ParamId {
+		MUTE,
+		MUTE1,
+		MUTE2,
+		MUTE3,
+		MUTE4,
+		MUTE5,
+		MUTE6,
+		MUTE7,
+		MUTE8,
+		TIME_SIG,
+		TIME_SIG1,
+		TIME_SIG2,
+		TIME_SIG3,
+		TIME_SIG4,
+		TIME_SIG5,
+		TIME_SIG6,
+		TIME_SIG7,
+		TIME_SIG8,
 		PARAMS_LEN
 	};
 	enum InputId {
-		VOLTAGE_IN,
+		IN,
+		IN2,
+		IN3,
+		IN4,
+		IN5,
+		IN6,
+		IN7,
+		IN8,
 		INPUTS_LEN
 	};
 	enum OutputId {
-		OUTPUT,
+		OUT,
+		OUT2,
+		OUT3,
+		OUT4,
+		OUT5,
+		OUT6,
+		OUT7,
+		OUT8,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -27,14 +59,14 @@ struct SyncMute : QuestionableModule {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		//configSwitch(RANGE_PARAM, 1.f, 8.f, 1.f, "Range", {"1", "2", "3", "4", "5", "6", "7", "8"});
 		//configParam(OFFSET_PARAM, 0.f, 8.f, 0.f, "Offset");
-		configInput(VOLTAGE_IN, "");
-		configOutput(OUTPUT, "");
+		configInput(IN, "");
+		configOutput(OUT, "");
 		
 	}
 
 	void process(const ProcessArgs& args) override {
 
-		outputs[OUTPUT].setVoltage(0);
+		//outputs[OUTPUT].setVoltage(0);
 
 	}
 
@@ -74,9 +106,14 @@ struct SyncMuteWidget : QuestionableWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<QuestionablePort<PJ301MPort>>(mm2px(Vec(7.8, 60)), module, SyncMute::VOLTAGE_IN));
 
-		addOutput(createOutputCentered<QuestionablePort<PJ301MPort>>(mm2px(Vec(7.8, 110)), module, SyncMute::OUTPUT));
+		for (size_t i = 0; i < 8; i++) {
+			addInput(createInputCentered<QuestionablePort<PJ301MPort>>(mm2px(Vec(7.8, 15 + (14* i))), module, SyncMute::IN + i));
+			addParam(createParamCentered<QuestionableParam<RoundSmallBlackKnob>>(Vec(18, 15 + (14* i)), module, SyncMute::TIME_SIG + i));
+			addParam(createParamCentered<QuestionableParam<CKD6>>(mm2px(Vec(20.2, 15 + (14* i))), module, SyncMute::MUTE + i));
+			addOutput(createOutputCentered<QuestionablePort<PJ301MPort>>(mm2px(Vec(32.8, 15 + (14 * i))), module, SyncMute::OUT + i));
+		}
+
 	}
 
 };
